e93ab453ab39cbf500d7e641164e20b077124608
Antiesh <overlorddarkys@gmail.com>
2022-04-14 10:54:47 +0300

    Добавление атрибутов

--- ConsoleApp1/ConsoleApp1/Address.cs
+++ ConsoleApp1/ConsoleApp1/Address.cs
@@ -6,5 +6,41 @@ namespace ConsoleApp1
 {
     class Address
     {
+        private string city;
+        private string street;
+        private int house;
+        public string City
+        {
+            get
+            {
+                return this.city;
+            }
+            set
+            {
+                this.city = value;
+            }
+        }
+        public string Street
+        {
+            get
+            {
+                return this.street;
+            }
+            set
+            {
+                this.street = value;
+            }
+        }
+        public int House
+        {
+            get
+            {
+                return this.house;
+            }
+            set
+            {
+                this.house = value;
+            }
+        }
     }
 }
